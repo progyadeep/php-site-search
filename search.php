58dcb05eaea122059951a6c566226af47f2a85c9
Progyadeep Moulik <progyadeep@dbzmail.com>
2019-12-11 11:25:35 +0530

    Update search.php

--- search.php
+++ search.php
@@ -1,43 +1,54 @@
 <?php
-	$q_words = explode(",", $_GET['q']);
-	$ignore_list = explode("\n", file_get_contents('ignore'));
+	$q_words = explode(",", $_GET['q']); //holds supplied keywords
+	$ignore_list = explode("\n", file_get_contents('ignore')); //holds files/folders to ignore in search
+	$score_tab = []; //will hold the search scores of files found on website
+	$sub = []; //will hold list of folders found on the website
 	
 	//initial list of files/folders to include in search scope
-	$search_index = array_diff(scandir(getcwd()), [".", ".."]);
-	$search_index = array_diff($search_index, $ignore_list);
+	$search_index = array_diff(scandir(getcwd()), [".", ".."], $ignore_list);
 	
 	//finding out folders in root directory
-	$sub = [];
-	
-	foreach($search_index as $s){
-		if(is_dir($s))
+	//the goal is to have only 'files' in $search_index
+	$l = count($search_index);
+	for($i=0; $i<$l; $i++){
+		$s = $search_index[$i];
+		if(is_dir($s)){
 			array_push($sub, $s);
+			unset($search_index[$i]);
+		}
 	}
 	
-	$search_index = array_diff($search_index, $sub);
-	
-	//finding all "files" in all sub-folders across the site
+	//finding all "files" in all folders/sub-folders across the site
+	//and calculating their search score
+
 	for($i=0; count($sub)>0; $i++){
 		$s = $sub[$i];
 		$t_s = array_diff(scandir($s), [".", ".."]);
 		foreach($t_s as $t){
 			if(!in_array($s."/".$t, $ignore_list)){
 				if(is_dir($s."/".$t))
-					array_push($sub, $s."/".$t);
+					array_push($sub, $s."/".$t); //it's a folder
 				else
-					array_push($search_index, $s."/".$t);
+					$score = calcScore($s."/".$t); //it's a file!
 			}
 		}
 		unset($sub[$i]);
 	}
-	//print_r($search_index)."\n";
+		
+	//calulating score of initial $search_index elements
+	foreach($search_index as $s)
+		calcScore($s);
+
+	//sorting score table in descending order of scores
+	arsort($score_tab);
 	
-	//Search index built, now will read meta keywords and calculate
-	//search score
-	$score_tab = [];
-	
-	foreach($search_index as $s){
-		$raw_c = file_get_contents($s);
+	//giving results as JSON array
+	echo str_replace("\\", "", json_encode($score_tab));
+
+	function calcScore($f){
+		global $q_words;
+		global $score_tab;
+		$raw_c = file_get_contents($f);
 		$si = strpos($raw_c, "<meta name=\"keywords\" content=\"")+31;
 		$kw = preg_split("/(,\s*+)/", substr($raw_c, $si, strpos($raw_c, "\"/>", $si)-$si));
 		
@@ -47,11 +58,8 @@
 				$score++;
 			}
 		}
+		
 		if($score > 0)
-			$score_tab[$s] = $score;
+			$score_tab[$f] = $score;
 	}
-	arsort($score_tab);
-	
-	//giving results as JSON array
-	echo str_replace("\\", "", json_encode($score_tab));
 ?>
